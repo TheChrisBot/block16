177d5739bac1809027fbf5a2ca92c89eda98ce46
Chris Strother <61249090+TheChrisBot@users.noreply.github.com>
2023-05-01 17:05:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+//subscription gets a 25% discount after total
+//coupons give $10 discount after the subscription discount has been calculated
+
+//the total for Timmy should be $65
+const timmy = {
+  prescription: "acetaminophen",
+  pricePerRefill: 25,
+  refills: 3,
+  subscription: false,
+  coupon: true,
+};
+//the total for Sarah should be $37.5
+const sarah = {
+  prescription: "diphenhydramine",
+  pricePerRefill: 50,
+  refills: 1,
+  subscription: true,
+  coupon: false,
+};
+//the total for Rocky should be $102.5
+const rocky = {
+  prescription: "phenylephrine",
+  pricePerRefill: 30,
+  refills: 5,
+  subscription: true,
+  coupon: true,
+};
+//create a function to calculate the totals with discounts included
+function calculateTotal(customer) {
+  let total = customer.pricePerRefill * customer.refills;
+
+  if (customer.subscription) {
+    total *= 0.75;
+  }
+
+  if (customer.coupon) {
+    total -= 10;
+  }
+
+  return total;
+}
+//assigning the totals to each name
+const timmyTotal = calculateTotal(timmy);
+const sarahTotal = calculateTotal(sarah);
+const rockyTotal = calculateTotal(rocky);
+//and here we will write out a solution to show the final totals
+const finalAmount = timmyTotal + sarahTotal + rockyTotal;
+console.log(`Your grand total is $${finalAmount}.`);
